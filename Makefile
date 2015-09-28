e743c1d64a5973e59d38f3506ebb3fd8568300d1
EKR <ekr@rtfm.com>
2015-09-28 10:35:48 -0700

    Allow proofs to fail

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ DEPS= $(wildcard *.m4i)
 M4= $(patsubst %.m4,%.spthy,$(wildcard *.m4))
 PROOFS= $(patsubst %.m4,%.proof,$(wildcard *.m4))
 
+ifdef ALLOW_FALSIFICATION
+ARGS += -a
+endif
+
 all: proofs
 
 proofs: $(PROOFS)
@@ -13,4 +17,4 @@ clean:
 	m4 $(FLAGS) -I. $< > $@
 
 %.proof: %.spthy
-	python ./run-tamarin.py $<
+	python ./run-tamarin.py $(ARGS) $<
--- run-tamarin.py
+++ run-tamarin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 import sys
+import argparse
 
 TAMARIN_PATH='tamarin-prover'
 FALSIFIED=False
@@ -8,7 +9,15 @@ FALSIFIED=False
 NOT_WELL_FORMED_MESSAGE='wellformedness check failed'
 FALSIFIED_MESSAGE='falsified - found trace'
 
-p = subprocess.Popen([TAMARIN_PATH, '--prove', sys.argv[1]],
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--allow_falsification",
+                    action="store_true")
+parser.add_argument("theory", 
+                    help="the theory to prove")
+args = parser.parse_args();
+
+p = subprocess.Popen([TAMARIN_PATH, '--prove', args.theory],
                      stdout=subprocess.PIPE,
                      stderr=subprocess.STDOUT)
 for l in p.stdout:
@@ -19,8 +28,9 @@ for l in p.stdout:
     if (l.find(FALSIFIED_MESSAGE) != -1):
         FALSIFIED=True
 
-if FALSIFIED:
-    sys.exit(1)
+if not args.allow_falsification:
+    if FALSIFIED:
+        sys.exit(1)
 
 p.wait()
 if p.returncode != 0:
